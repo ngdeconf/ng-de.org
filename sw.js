84e7ec96ba4302a2e22f11efa533c41353368be4
Robin Böhm <robin.boehm@workshops.de>
2024-05-07 13:53:32 +0200

    feat(sw): change file to cache list

--- sw.js
+++ sw.js
@@ -2,9 +2,38 @@
 layout: null
 ---
 
-navigator.serviceWorker.getRegistrations().then(registrations => {
-  for (const registration of registrations) {
-      registration.unregister();
-      location.reload();
-  } 
-});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+const version = '{{site.time | date: '%Y%m%d%H%M%S'}}';
+const staticCacheName = `static::${version}`;
+
+console.log("installing service worker");
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
+  /.*\.css/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'css-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
+const filesToCache = [
+  
+];
