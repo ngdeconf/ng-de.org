5c9e14ff2cfba26a71863b7bc3f23ce02c8e09ff
Robin Böhm <robin.boehm@workshops.de>
2024-05-07 13:49:53 +0200

    feat(sw): delete sw from within

--- sw.js
+++ sw.js
@@ -2,55 +2,9 @@
 layout: null
 ---
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
-
-const version = '{{site.time | date: '%Y%m%d%H%M%S'}}';
-const staticCacheName = `static::${version}`;
-
-console.log("installing service worker");
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.networkFirst()
-);
-
-workbox.routing.registerRoute(
-  /.*\.css/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'css-cache',
-  })
-);
-
-workbox.routing.registerRoute(
-  /.*\.(?:png|jpg|jpeg|svg|gif)/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'image-cache',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20,
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      })
-    ],
-  })
-);
-
-const filesToCache = [
-  '/',
-  {%- for page in site.html_pages %}
-    {%- unless page.url == '/' %}
-  '{{ page.url }}',
-    {%- endunless %}
-  {%- endfor %}
-  {%- for post in site.posts %}
-  '{{ post.url }}',
-  {%- endfor %}
-  {%- for file in site.static_files %}
-    {%- if
-      file.extname == '.js' or
-      file.path contains '/assets/sponsors'
-    %}
-  '{{ file.path }}',
-    {%- endif %}
-  {%- endfor %}
-  'css/default.css'
-];
+navigator.serviceWorker.getRegistrations().then(registrations => {
+  for (const registration of registrations) {
+      registration.unregister();
+      location.reload();
+  } 
+});
