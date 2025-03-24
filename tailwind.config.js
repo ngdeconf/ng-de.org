173c190e6c6147f43943cee49b94f164feccf386
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-03-24 20:36:29 +0100

    chore: remove node_modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   darkMode: 'class',
   theme: {
     extend: {
@@ -19,7 +17,7 @@ module.exports = {
           600: '#B80029',
           700: '#930020',
           800: '#6E0018',
-          900: '#49000F',
+          900: '#49000F'
         },
         secondary: {
           DEFAULT: '#C3002F',
@@ -32,23 +30,24 @@ module.exports = {
           600: '#A00026',
           700: '#7D001E',
           800: '#5A0016',
-          900: '#37000E',
+          900: '#37000E'
         },
         // Berlin colors
         berlin: {
           red: '#E03232',
           black: '#202020',
-          yellow: '#FFCE00',
+          yellow: '#FFCE00'
         },
-        accent: '#3700B3', // Angular accent color
+        accent: '#3700B3' // Angular accent color
       },
       fontFamily: {
-        sans: ['"Inter"', 'sans-serif'],
+        sans: ['"Inter"', 'sans-serif']
       },
       backgroundImage: {
-        'berlin-pattern': "url('https://images.unsplash.com/photo-1560930950-5cc20e80e392?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80')",
-      },
-    },
+        'berlin-pattern':
+          "url('https://images.unsplash.com/photo-1560930950-5cc20e80e392?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80')"
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
