1be9fa9ded3a2cd212469465bf920e4b52d40d81
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-03-25 08:32:06 +0100

    wip(ssr): try to make ssr work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
-import { TicketsComponent } from './components/tickets/tickets.component';
-import { SpeakersComponent } from './components/speakers/speakers.component';
 import { ScheduleComponent } from './components/schedule/schedule.component';
+import { SpeakersComponent } from './components/speakers/speakers.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
 import { WorkshopsComponent } from './components/workshops/workshops.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
+    RouterOutlet,
     HeaderComponent,
     HeroComponent,
     TicketsComponent,
@@ -22,14 +24,12 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   template: `
-    <div class="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+    <div
+      class="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100"
+    >
       <app-header />
       <main>
-        <app-hero />
-        <app-tickets />
-        <app-speakers />
-        <app-schedule />
-        <app-workshops />
+        <router-outlet />
       </main>
       <app-footer />
     </div>
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
+import { ScheduleComponent } from '../schedule/schedule.component';
+import { SpeakersComponent } from '../speakers/speakers.component';
+import { TicketsComponent } from '../tickets/tickets.component';
+import { WorkshopsComponent } from '../workshops/workshops.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HeroComponent,
+    TicketsComponent,
+    SpeakersComponent,
+    ScheduleComponent,
+    WorkshopsComponent
+  ],
+  template: `
+    <app-hero />
+    <app-tickets />
+    <app-speakers />
+    <app-schedule />
+    <app-workshops />
+  `
+})
+export class HomeComponent {}
