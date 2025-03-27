8d99c87c559a9bc248483a5a208d99fc3bb992f9
Robin Böhm <robin.boehm@workshops.de>
2025-03-27 11:16:07 +0100

    feat(code-of-conduct): add Code of Conduct component and routing; update footer to link to the new page

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,9 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'code-of-conduct',
+    renderMode: RenderMode.Prerender
   }
 ];
