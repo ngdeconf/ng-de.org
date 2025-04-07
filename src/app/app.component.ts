f3322c04fca896501b9268bd06e75418d218727f
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-04-07 15:08:04 +0200

    feat(hero): add impressions video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/page-sections/footer.component';
 import { TopNavigationComponent } from './components/page-sections/top-navigation.component';
 
 @Component({
   selector: 'ngde-root',
-  imports: [RouterModule, TopNavigationComponent, FooterComponent],
+  imports: [RouterOutlet, TopNavigationComponent, FooterComponent],
   template: `
     <div
       class="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100"
